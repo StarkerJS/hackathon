d780d4dc01225d037754df0a41046a8b0862e29f
StarkerJS <antk.swallow@gmail.com>
2019-11-08 14:39:04 +0900

    committed in 2019/11/08 14:39:04

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,8 +3,11 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = $('userName').val();
-
+    const userName = document.getElementById('userName').value;
+    // ユーザ名が未入力でないかチェックする
+    if (userName !=""){
+      alart("ERR0R")
+    }else{
     $('form').submit();
-	}
+    }
 }
