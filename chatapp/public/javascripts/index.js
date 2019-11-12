0d8af823d6ac5bd8dc981b3bc17e9e442791f5ce
StarkerJS <antk.swallow@gmail.com>
2019-11-12 15:15:25 +0900

    committed in 2019/11/12 15:15:25

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -5,9 +5,9 @@ function enter() {
     // 入力されたユーザ名を取得する
     const userName = document.getElementById('userName').value;
     // ユーザ名が未入力でないかチェックする
-    if (userName !=""){
+    if (userName !==""){
       $('form').submit();
     }else{
-    alart("ERR0R")
+    alart('ERR0R')
     }
 }
