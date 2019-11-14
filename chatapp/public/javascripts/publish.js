9beee290991a4f1ee20f290dc0f99c07e2f4ec47
hirokichinaga <hirokichinaga@yahoo.co.jp>
2019-11-14 18:59:06 +0900

    committed in 2019/11/14 18:59:06

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -1,17 +1,28 @@
 'use strict';
 
+function getValueN() {
+    const textNValue = $('#userName').val();
+    return textNValue;
+}
+
+function getValueM() {
+    const textMValue = $('#message').val();
+    return textMValue;
+}
+
 // 投稿メッセージをサーバに送信する
 function publish() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = getValueN();
     // 入力されたメッセージを取得
-    const message = '';
+    const message = getValueM();
     // 投稿内容を送信
-
+    socket.emit('sendNameEvent', userName);
+    socket.emit('sendMessageEvent', message);
     return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('receiveMessageEvent', function (data) {
+    $('#thread').prepend('<p>' + data +'</p>');
 });
