525a302a97f36fac61733d75a3adb27f0e5c3c11
ookuma-taka0924 <ookuma.taka0924@gmail.com>
2019-11-16 00:54:17 +0900

    committed in 2019/11/16 00:54:17

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -3,11 +3,11 @@
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = $('#userName').val();
+    //const userName = $('#userName').val();
     // 入力されたメッセージを取得
     const message = $('#message').val();
     // メモの内容を表示
-    $('#thread').prepend('<p>' + userName +'さん：' + message +'</p>');
+    $('#thread').prepend('<p>memo＞＞' + message +'</p>');
 
     return false;
 }
